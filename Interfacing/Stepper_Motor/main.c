1fc88eb74a755ef049e2d48c7a226026bf3c647d
Adel1250 <adelashrafhamouda@gmail.com>
2022-12-23 18:22:26 +0200

    Add modes in stepper motor

--- Interfacing/Stepper_Motor/main.c
+++ Interfacing/Stepper_Motor/main.c
@@ -5,22 +5,86 @@
 
 int main(void) {
     dioSetPortDirection(3, OUTPUT);
-    dioSetPortDirection(3, OUTPUT);
-    dioSetPortDirection(3, OUTPUT);
-    dioSetPortDirection(3, OUTPUT);
+
+    dioSetPinDirection(0, 0, INPUT);
+    dioSetPinDirection(1, 0, INPUT);
+    dioSetPinDirection(2, 0, INPUT);
+    dioSetPinDirection(3, 0, INPUT);
+
+    dioSetPinValue(0, 0, 1);
+    dioSetPinValue(1, 0, 1);
+    dioSetPinValue(2, 0, 1);
+    dioSetPinValue(3, 0, 1);
 
     while (1) {
-        for (u8 i = 0; i < 128; i++) {
-            dioSetPortValue(3, 0x03);
-            _delay_ms(50);
-            dioSetPortValue(3, 0x06);
-            _delay_ms(50);
-            dioSetPortValue(3, 0x0C);
-            _delay_ms(50);
-            dioSetPortValue(3, 0x09);
-            _delay_ms(50);
+        if (dioGetPinValue(0, 0) == 0) {
+            for (u8 i = 0; i < 128; i++) {
+                dioSetPortValue(3, 0x03);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x06);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x0C);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x09);
+                _delay_ms(50);
+            }
+            _delay_ms(1000);
         }
-        _delay_ms(1000);
+        else if (dioGetPinValue(1, 0) == 0) {
+            for (u8 i = 0; i < 128; i++) {
+                dioSetPortValue(3, 0x01);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x02);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x04);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x08);
+                _delay_ms(50);
+            }
+            _delay_ms(1000);
+        }
+        else if (dioGetPinValue(2, 0) == 0) {
+            for (u8 i = 0; i < 64; i++) {
+                dioSetPortValue(3, 0x01);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x03);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x02);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x06);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x04);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x0C);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x08);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x09);
+                _delay_ms(50);
+            }
+            _delay_ms(1000);
+        }
+        else if (dioGetPinValue(3, 0) == 0) {
+            for (u8 i = 0; i < 3; i++) {
+                dioSetPortValue(3, 0x01);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x03);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x02);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x06);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x04);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x0C);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x08);
+                _delay_ms(50);
+                dioSetPortValue(3, 0x09);
+                _delay_ms(50);
+            }
+            _delay_ms(1000);
+        }
     }
     return 0;
 }
